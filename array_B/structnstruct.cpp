1c379d86a3ca39bc85dae324ed6a9cf1c3f9243e
suhilmi01 <suhilmi01@mail.com>
2026-04-07 15:32:14 +0700

    objek

--- array_B/structnstruct.cpp
+++ array_B/structnstruct.cpp
@@ -10,3 +10,17 @@ struct orang{
     DetailAlamat alamat;
     int umur;
 };
+int main() {
+    // Deklarasi obyek dari struct orang
+    orang mhs;
+
+    //isi objek
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Alamat Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
