10b017db4792c7aed519540a37db2c85b9dfdc1a
suhilmi01 <suhilmi01@mail.com>
2026-04-07 14:58:41 +0700

    deklarasi tanpa inisialisasi

--- array_B/array.cpp
+++ array_B/array.cpp
@@ -4,4 +4,7 @@ using namespace std;
 int main(){
     //deklarasi array dengan inisialisasi
     int dataku[5] = {10,20,30,40,50};
+    //deklarasi tanpa inisialisasi
+    int datamu[5];
+    int x;
 }
