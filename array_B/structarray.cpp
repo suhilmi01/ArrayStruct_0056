87b0f8b63997185cfeb4765f2f931ef90cc8fec5
suhilmi01 <suhilmi01@mail.com>
2026-04-07 15:43:19 +0700

    objek

--- array_B/structarray.cpp
+++ array_B/structarray.cpp
@@ -10,3 +10,22 @@ struct orang{
     DetailAlamat alamat;
     int umur;
 };
+int main() {
+    // Deklarasi obyek dari struct orang
+    orang mhs[3];
+
+    //Mengisi objek
+        for (int i = 0; i <= 2; i++) 
+        {
+            cout << "Nama Mahasiswa: ";
+            getline(cin, mhs[i].nama);
+            cout << "Alamat Desa: ";
+            getline(cin, mhs[i].alamat.desa);
+            cout << "Alamat Kota: ";
+            getline(cin, mhs[i].alamat.kota);
+            cout << "Umur: ";
+            cin >> mhs[i].umur;
+            cin.ignore(); // Mengabaikan karakter newline setelah input umur
+            cout << endl;
+        }
+        cout << endl;
