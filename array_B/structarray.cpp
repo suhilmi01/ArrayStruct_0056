f67edc7218712bb508dede488895f82541bb0f94
suhilmi01 <suhilmi01@mail.com>
2026-04-07 15:42:32 +0700

    structarray

---
+++ array_B/structarray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+struct orang{
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
